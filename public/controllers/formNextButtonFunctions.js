ab3f9898411174f92b11d62e134b0a67eec06278
Rolando Mora <rolormg@gmail.com>
2023-08-14 23:11:17 -0600

    Agregando mejoras al api

--- public/controllers/formNextButtonFunctions.js
+++ public/controllers/formNextButtonFunctions.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var nextBtn = document.getElementById('nextBtn');
   
-    nextBtn.addEventListener('click', function(event) {
+    nextBtn.addEventListener('click', async function(event) {
       event.preventDefault(); // Evita la acción por defecto del botón
   
       var fields = document.querySelectorAll('#form1 [required]');
@@ -27,10 +27,10 @@ document.addEventListener('DOMContentLoaded', function() {
       } else {
 
         var emailBox = document.getElementById('email');
-        var email = emailBox.value.trim();
+        var emailB = emailBox.value.trim();
         var regex = /^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-z]+$/;
 
-        if (!regex.test(email)) {
+        if (!regex.test(emailB)) {
           emailBox.style.borderColor = 'red';
           Swal.fire({
             icon: 'warning',
@@ -42,10 +42,10 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
 
           var idBox = document.getElementById('id');
-          var id= idBox.value.trim();
+          var idB= idBox.value.trim();
           var idRegex = /^\d{9}$/;
 
-          if (!idRegex.test(id)) {
+          if (!idRegex.test(idB)) {
             idBox.style.borderColor = 'red';
             Swal.fire({
               icon: 'warning',
@@ -57,10 +57,10 @@ document.addEventListener('DOMContentLoaded', function() {
           }
 
           var phoneBox = document.getElementById('phone');
-          var phone = phoneBox.value.trim();
+          var phoneB = phoneBox.value.trim();
           var phoneRegex = /^\d{11}$/;
 
-          if (!phoneRegex.test(phone)) {
+          if (!phoneRegex.test(phoneB)) {
             phoneBox.style.borderColor = 'red';
             Swal.fire({
               icon: 'warning',
@@ -72,10 +72,10 @@ document.addEventListener('DOMContentLoaded', function() {
           }
 
           var passwordBox = document.getElementById('password');
-          var password = passwordBox.value.trim();
+          var passwordB = passwordBox.value.trim();
           var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-          if (!passwordRegex.test(password)) {
+          if (!passwordRegex.test(passwordB)) {
             passwordBox.style.borderColor = 'red';
             Swal.fire({
               icon: 'warning',
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
           var passwordCheckBox = document.getElementById('password-check');
           var passwordCheck = passwordCheckBox.value.trim();
 
-          if (password !== passwordCheck) {
+          if (passwordB !== passwordCheck) {
             passwordCheckBox.style.borderColor = 'red';
             Swal.fire({
               icon: 'warning',
@@ -100,6 +100,63 @@ document.addEventListener('DOMContentLoaded', function() {
             return; // Detiene la ejecución si las contraseñas no coinciden
           }
 
+          const formData = new FormData(document.querySelector('.finaluserform__personalinfo__form'));
+  
+          const name = formData.get('name');
+          const last = formData.get('lastname');
+          const id = formData.get('id');
+          const phone = formData.get('phone');
+          const email = formData.get('email');
+          const password = formData.get('password');
+          const province = formData.get('provincia');
+          const canton = formData.get('canton');
+          const district = formData.get('distrito');
+          const country = formData.get('country');
+          const gender = formData.get('gender');
+          const personalDetails = formData.get('personalDetails');
+          try {
+            const response = await axios.post('http://localhost:3000/api/users.routes', {
+              name,
+              last,
+              id,
+              phone,
+              email,
+              password,
+              province,
+              canton,
+              district,
+              country,
+              gender,
+              personalDetails
+          });
+
+          if (response.data.result) {
+            Swal.fire({
+              title: 'Persona registrada exitosamente',
+              icon: 'success'
+            });
+          } else {
+            // Manejar error en el registro
+            switch (response.data.error.code) {
+              case 11000:
+                Swal.fire({
+                  title: 'No se completó el registro',
+                  text: 'La persona ya está registrada',
+                  icon: 'warning'
+                });
+                break;
+                // Manejar otros posibles errores
+              }
+            }
+          } catch (error) {
+            console.log(error);
+            // Mostrar mensaje de error genérico en caso de falla en la solicitud
+            Swal.fire({
+              title: 'Error',
+              text: 'Ocurrió un error al registrar la persona',
+              icon: 'error'
+            });
+          }
           // Si no hay campos faltantes y todos los datos son válidos, oculta el formulario actual y muestra el siguiente
           var nextForm = document.getElementById('form2');
           fields.forEach(function(field) {
--- public/controllers/userRegistration.js
+++ public/controllers/userRegistration.js
@@ -1,62 +1,62 @@
-const nextBtn = document.getElementById('nextBtn');
+// const nextBtn = document.getElementById('nextBtn');
 
-nextBtn.addEventListener('click', async () => {
-  const formData = new FormData(document.querySelector('.finaluserform__personalinfo__form'));
+// nextBtn.addEventListener('click', async () => {
+//   const formData = new FormData(document.querySelector('.finaluserform__personalinfo__form'));
   
-  const name = formData.get('name');
-  const last = formData.get('lastname');
-  const id = formData.get('id');
-  const phone = formData.get('phone');
-  const email = formData.get('email');
-  const password = formData.get('password');
-  const province = formData.get('provincia');
-  const canton = formData.get('canton');
-  const district = formData.get('distrito');
-  const country = formData.get('country');
-  const gender = formData.get('gender');
-  const personalDetails = formData.get('personalDetails');
+//   const name = formData.get('name');
+//   const last = formData.get('lastname');
+//   const id = formData.get('id');
+//   const phone = formData.get('phone');
+//   const email = formData.get('email');
+//   const password = formData.get('password');
+//   const province = formData.get('provincia');
+//   const canton = formData.get('canton');
+//   const district = formData.get('distrito');
+//   const country = formData.get('country');
+//   const gender = formData.get('gender');
+//   const personalDetails = formData.get('personalDetails');
 
-  try {
-    const response = await axios.post('http://localhost:3000/api/users.routes', {
-      name,
-      last,
-      id,
-      phone,
-      email,
-      password,
-      province,
-      canton,
-      district,
-      country,
-      gender,
-      personalDetails
-    });
+//   try {
+//     const response = await axios.post('http://localhost:3000/api/users.routes', {
+//       name,
+//       last,
+//       id,
+//       phone,
+//       email,
+//       password,
+//       province,
+//       canton,
+//       district,
+//       country,
+//       gender,
+//       personalDetails
+//     });
 
-    if (response.data.result) {
-      Swal.fire({
-        title: 'Persona registrada exitosamente',
-        icon: 'success'
-      });
-    } else {
-      // Manejar error en el registro
-      switch (response.data.error.code) {
-        case 11000:
-          Swal.fire({
-            title: 'No se completó el registro',
-            text: 'La persona ya está registrada',
-            icon: 'warning'
-          });
-          break;
-        // Manejar otros posibles errores
-      }
-    }
-  } catch (error) {
-    console.log(error);
-    // Mostrar mensaje de error genérico en caso de falla en la solicitud
-    Swal.fire({
-      title: 'Error',
-      text: 'Ocurrió un error al registrar la persona',
-      icon: 'error'
-    });
-  }
-});
+//     if (response.data.result) {
+//       Swal.fire({
+//         title: 'Persona registrada exitosamente',
+//         icon: 'success'
+//       });
+//     } else {
+//       // Manejar error en el registro
+//       switch (response.data.error.code) {
+//         case 11000:
+//           Swal.fire({
+//             title: 'No se completó el registro',
+//             text: 'La persona ya está registrada',
+//             icon: 'warning'
+//           });
+//           break;
+//         // Manejar otros posibles errores
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     // Mostrar mensaje de error genérico en caso de falla en la solicitud
+//     Swal.fire({
+//       title: 'Error',
+//       text: 'Ocurrió un error al registrar la persona',
+//       icon: 'error'
+//     });
+//   }
+// });
