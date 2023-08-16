f2c0dc26c0b26b7d9768af75b636f8c94b8df7b5
Rolando Mora <rolormg@gmail.com>
2023-08-15 21:11:17 -0600

    Agregando Base de Datos para registro de Usuarios(Colaboradores)

--- api/models/Users.js
+++ api/models/Users.js
@@ -18,7 +18,27 @@ const schema_users = new mongoose.Schema({
     country: {type: String, required: true, unique: false},
     gender: {type: String, required: true, unique: false},
     personalDetails: {type: String, required: true, unique: false},
-    status: {type: String, default: 0}
+    academicTable: [
+        {
+        academicSchool: {type: String, required: false},
+        academicArea: {type: String, required: false},
+        academicLevel: {type: String, required: false},
+        academicStatus: {type: String, required: false},
+        academicMonthFrom: {type: String, required: false},
+        academicYearFrom: {type: String, required: false},
+        academicMonthTo: {type: String, required: false},
+        academicYearTo: {type: String, required: false}
+        }
+    ],
+    jobTable: [{
+        jobEnterpriseName: {type: String, required: false},
+        jobType: {type: String, required: false},
+        jobDescription: {type: String, required: false},
+        jobMonthFrom: {type: String, required: false},
+        jobYearFrom: {type: String, required: false},
+        jobMonthTo: {type: String, required: false},
+        jobYearTo: {type: String, required: false}
+    }]
 })
 
 
--- public/controllers/userRegistration.js
+++ public/controllers/userRegistration.js
@@ -1,62 +1,109 @@
-// const nextBtn = document.getElementById('nextBtn');
+const saveProfileBtn = document.getElementById('saveProfileBtn');
 
-// nextBtn.addEventListener('click', async () => {
-//   const formData = new FormData(document.querySelector('.finaluserform__personalinfo__form'));
+saveProfileBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(document.querySelector('.finaluserform__personalinfo__form'));
   
-//   const name = formData.get('name');
-//   const last = formData.get('lastname');
-//   const id = formData.get('id');
-//   const phone = formData.get('phone');
-//   const email = formData.get('email');
-//   const password = formData.get('password');
-//   const province = formData.get('provincia');
-//   const canton = formData.get('canton');
-//   const district = formData.get('distrito');
-//   const country = formData.get('country');
-//   const gender = formData.get('gender');
-//   const personalDetails = formData.get('personalDetails');
+    const name = formData.get('name');
+    const last = formData.get('lastname');
+    const id = formData.get('id');
+    const phone = formData.get('phone');
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const province = formData.get('provincia');
+    const canton = formData.get('canton');
+    const district = formData.get('distrito');
+    const country = formData.get('country');
+    const gender = formData.get('gender');
+    const personalDetails = formData.get('personalDetails');
 
-//   try {
-//     const response = await axios.post('http://localhost:3000/api/users.routes', {
-//       name,
-//       last,
-//       id,
-//       phone,
-//       email,
-//       password,
-//       province,
-//       canton,
-//       district,
-//       country,
-//       gender,
-//       personalDetails
-//     });
+    const jobEnterpriseName = formData.get('jobEnterpriseName');
+    const jobType = formData.get('jobType');
+    const jobDescription = formData.get('jobDescription');
+    const jobMonthFrom = formData.get('jobMonthFrom');
+    const jobYearFrom = formData.get('jobYearFrom');
+    const jobMonthTo = formData.get('jobMonthTo');
+    const jobYearTo = formData.get('jobYearTo');
 
-//     if (response.data.result) {
-//       Swal.fire({
-//         title: 'Persona registrada exitosamente',
-//         icon: 'success'
-//       });
-//     } else {
-//       // Manejar error en el registro
-//       switch (response.data.error.code) {
-//         case 11000:
-//           Swal.fire({
-//             title: 'No se completó el registro',
-//             text: 'La persona ya está registrada',
-//             icon: 'warning'
-//           });
-//           break;
-//         // Manejar otros posibles errores
-//       }
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     // Mostrar mensaje de error genérico en caso de falla en la solicitud
-//     Swal.fire({
-//       title: 'Error',
-//       text: 'Ocurrió un error al registrar la persona',
-//       icon: 'error'
-//     });
-//   }
-// });
+    // Capturar datos de la tabla
+    const academicTableRows = document.querySelectorAll('#table-academic tbody tr');
+    const jobTableRows = document.querySelectorAll('#table-jobExperience tbody tr');
+    const academicData = [];
+    const jobData = [];
+    
+
+    academicTableRows.forEach(row => {
+        const cells = row.querySelectorAll('td');
+        const rowData = Array.from(cells).map(cell => cell.textContent);
+        academicData.push({
+        academicSchool: rowData[0],
+        academicArea: rowData[1],
+        academicLevel: rowData[2],
+        academicStatus: rowData[3],
+        academicMonthFrom: rowData[4],
+        academicYearFrom: rowData[5],
+        academicMonthTo: rowData[6],
+        academicYearTo: rowData[7]
+        });
+    });
+
+    jobTableRows.forEach(row => {
+        const cells = row.querySelectorAll('td');
+        const rowData = Array.from(cells).map(cell => cell.textContent);
+        jobData.push({
+            jobEnterpriseName: rowData[0],
+            jobType: rowData[1],
+            jobDescription: rowData[2],
+            jobMonthFrom: rowData[3],
+            jobYearFrom: rowData[4],
+            jobMonthTo: rowData[5],
+            jobYearTo: rowData[6]
+        });
+    });
+
+    try {
+        const response = await axios.post('http://localhost:3000/api/users.routes', {
+        name,
+        last,
+        id,
+        phone,
+        email,
+        password,
+        province,
+        canton,
+        district,
+        country,
+        gender,
+        personalDetails,
+        academicTable: academicData,
+        jobTableRows: jobData
+        });
+
+        if (response.data.result) {
+        Swal.fire({
+            title: 'Se ha registrado exitosamente',
+            icon: 'success'
+        });
+        } else {
+        // Manejar error en el registro
+        switch (response.data.error.code) {
+            case 11000:
+            Swal.fire({
+                title: 'No se completó el registro',
+                text: 'La persona ya está registrada',
+                icon: 'warning'
+            });
+            break;
+            // Manejar otros posibles errores
+        }
+        }
+    } catch (error) {
+        console.log(error);
+        // Mostrar mensaje de error genérico en caso de falla en la solicitud
+        Swal.fire({
+        title: 'Error',
+        text: 'Ocurrió un error al registrar la persona',
+        icon: 'error'
+        });
+    }
+});
